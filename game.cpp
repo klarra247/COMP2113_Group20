7847edd3307758da67fc026b38ec08dd198337e5
klarra <klarra247@gmail.com>
2023-04-26 02:05:10 +0800

    game.cpp : game function update

---
+++ game.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+
+int playGame(string word) {
+    string input;
+    int count = 0;
+    while (count < 5) {
+        cout << "Enter a 5-letter word: ";
+        cin >> input;
+        if (input.length() != 5) {
+            cout << "Invalid input. Please enter a 5-letter word." << endl;
+            continue;
+        }
+        string correct = "";
+        string wrong = "";
+        string other = "";
+        for (int i = 0; i < 5; i++) {
+            if (input[i] == word[i]) {
+                correct += input[i];
+            }
+            else if (word.find(input[i],0) != string::npos) {
+                wrong += input[i];
+            }
+            else {
+                other += input[i];
+            }
+        }
+
+if (correct == input) {
+            cout << "Congratulations! You guessed the correct word!" << endl;
+            break;
+        }
+        else {
+            count++;
+            cout << "Incorrect guess. You have " << 5 - count << " guesses left." << endl;
+
+        cout << "Letters that are in the word and in the correct spot: " << correct << endl;
+        cout << "Letters that are in the word but in the wrong spot: " << wrong << endl;
+        cout << "All other alphabets: " << other << endl;
+
+            if (count == 5) {
+                cout << "Sorry, you have used up all your guesses." << endl;
+                break;
+            }
+        }
+
+
+        
+    }
+    return 0;
+}
+
+int main(){
+    playGame("hello");
+    return 0;
+}
