271055d18607ed579e755d8f8c924d003dd5151e
imsoheehee <125637658+imsoheehee@users.noreply.github.com>
2023-05-02 22:42:01 +0800

    Add files via upload

---
+++ game_setup.h
@@ -0,0 +1,17 @@
+#ifndef game_setup_H
+#define game_setup_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Wordle {
+void Contributors();
+void Player();
+void Start();
+void Exit();
+
+}
+
+#endif
+
