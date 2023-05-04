a3260311246a79c02146747af7c16b8af01bf9c3
klarra <klarra247@gmail.com>
2023-05-05 05:44:07 +0800

    update game.cpp, game.h

--- game.cpp
+++ game.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include<cstdlib> // for the rand(), srand() functions to generate random numbers
+#include "game.h"
+#include "setup.h"
+#include "players.h"
 
 using namespace std;
 
 
+//
+const int OTHER = 0;
+const int WRONG_POS = 1;
+const int CORRECT_POS = 2;
+//
 
 void checkCase(string& word) {
     for (int i = 0; i < word.length(); i++) {
@@ -15,56 +19,207 @@ void checkCase(string& word) {
     }
 }
 
-int playGame(string word) {
+//
+string getRandomWord(){
+    ifstream files("wordfile.txt");
+    vector<string> wordlist;
+    string words;
+    while (files >> words) {
+        checkCase(words);
+        wordlist.push_back(words);
+    }
+    files.close();
+    srand(time(NULL));
+    int randomNumber = rand() % wordlist.size();
+    return wordlist[randomNumber];
+}
+//
+
+
+//
+bool check_if_5_letters(const string& user_input){
+	for ( int i = 0; i < user_input.length(); i++)
+	{
+		if (isdigit(user_input[i]))
+			return false;
+	}
+	return (user_input.length() == 5);
+}
+
+bool spellcheck(const string& user_input){
+    //open text file with all valid words
+    string filename = "dictionary.txt";
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Error" << endl;
+        return false;
+    }
+
+    //add every word from txt file to vector
+    vector<string> words;
+    string word;
+    while (getline(file, word)) {
+        words.push_back(word);
+    }
+    file.close();
+
+    //binary search to see if word is valid 
+    int lower = 0;
+    int upper = words.size() - 1;
+    while (lower <= upper) {
+        int mid = (lower + upper) / 2;
+        if (words[mid] == user_input) {
+            return true;
+        } else if(words[mid] < user_input) {
+            lower = mid + 1;
+        } else if(words[mid] > user_input) {
+            upper = mid - 1;
+        }
+    }
+    return false;
+}
+//
+
+//
+void toLowerCase( string &input)
+{
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c)
+    { return tolower(c); });
+}
+//
+
+//
+void markMatch(vector<vector<int> > &matches, int tryIndex, string target, string guess)
+{
+    for (int i = 0; i < guess.length(); i++)
+    {
+        matches[tryIndex][i] = OTHER;
+    }
+    for (int j = 0; j < guess.length(); j++)
+    {
+        for (int i = 0; i < target.length(); i++)
+        {
+            if (guess[j] == target[i])
+            {
+                if (i == j)
+                {
+                    matches[tryIndex][j] = CORRECT_POS;
+                   break;
+                }
+                else
+                {
+                    matches[tryIndex][j] = WRONG_POS;
+                }
+            }
+        }
+    }
+
+
+}
+
+//
+void printWordle( vector< string> tries,  vector< vector<int>> matches, int currentTry)
+{
+    
+    for (int i = 0; i <= currentTry && i < tries.size(); i++)
+    {
+         string separator = "-";
+         string padding = "|";
+         string text = "|";
+        for (int j = 0; j < tries[i].length(); j++)
+        {
+            separator += "------";
+            padding += "     |";
+            char value =  toupper(tries[i][j]);
+            text += "  ";
+            if (matches[i][j] == WRONG_POS)
+            {
+                text += "\033[33m";
+            }
+            else if (matches[i][j] == CORRECT_POS)
+            {
+                text += "\033[32m";
+            }
+            text += value;
+            if (matches[i][j] == WRONG_POS || matches[i][j] == CORRECT_POS)
+            {
+                text += "\033[0m";
+            }
+            text += "  |";
+        }
+        if (i == 0)
+        { 
+             cout << separator <<  endl;
+        }
+         cout << padding <<  endl;
+         cout << text <<  endl;
+         cout << padding <<  endl; 
+         cout << separator <<  endl;
+    }
+}
+//
+//
+bool isAllMatch(string target, string guess)
+{
+    for (int i = 0; i < guess.length(); i++)
+    {
+        if (guess[i] != target[i])
+            return false;
+    }
+    return true;
+}
+//
+
+void playGame()
+{
+    //
+    int numberOfTries = 6;
+    vector<string> tries(numberOfTries);
+    vector<vector<int> > matches(numberOfTries, vector<int>(5));
+    string targetWord = getRandomWord();
+    toLowerCase(targetWord);   
     string input;
-    int count = 0;
-    while (count < 5) {
-        cout << "Enter a 5-letter word: ";
-        cin >> input;
-        if (input.length() != 5) {
-            cout << "Invalid input. Please enter a 5-letter word." << endl;
-            continue;
-        }
-        string correct = "";
-        string wrong = "";
-        string other = "";
-        for (int i = 0; i < 5; i++) {
-            if (input[i] == word[i]) {
-                correct += input[i];
-            }
-            else if (word.find(input[i],0) != string::npos) {
-                wrong += input[i];
-            }
-            else {
-                other += input[i];
-            }
-        }
+    int currentTry = 0; 
 
-if (correct == input) {
-            cout << "Congratulations! You guessed the correct word!" << endl;
-            break;
-        }
-        else {
-            count++;
-            cout << "Incorrect guess. You have " << 5 - count << " guesses left." << endl;
-
-        cout << "Letters that are in the word and in the correct spot: " << correct << endl;
-        cout << "Letters that are in the word but in the wrong spot: " << wrong << endl;
-        cout << "All other alphabets: " << other << endl;
-
-            if (count == 5) {
-                cout << "Sorry, you have used up all your guesses." << endl;
+    while (currentTry < numberOfTries){
+            cout << "Please enter your guess (word length must be 5) or type Q/q to quit: ";
+            getline(cin, input);
+        if(input != "q" && spellcheck(input) == true && check_if_5_letters(input) == true){
+            toLowerCase(input);
+            tries[currentTry] = input;
+            markMatch(matches, currentTry, targetWord, input);
+            printWordle(tries, matches, currentTry);
+            if (isAllMatch(targetWord, input))
+            {
+                cout << "Found the word" << endl;
+                
                 break;
             }
+            if (currentTry == 4)
+            {
+                cout << "You failed to find the word" << endl;
+                break;
+            }
+        } 
+        else if (input == "q"){
+            cout << "Quit game" <<  endl;
+            break;
+        }   
+        else if (check_if_5_letters(input) == false){
+            cout << "The input should be 5 words! Try Again."<<endl;
+            continue;
         }
+        else if (spellcheck(input) == false){
+            cout << "The word does not exist! Try Again."<<endl;
+            continue;
+        }
+      
 
-
-        
+ 
+        currentTry++;
     }
-    return 0;
+
 }
 
-int main(){
-    playGame("hello");
-    return 0;
-}
+
+
